c929cb710d3589f045baad7821708df4c77efff2
userspecialname <142438278+userspecialname@users.noreply.github.com>
2024-06-18 13:35:34 +0300

    Update ringinfo.c

--- ringinfo.c
+++ ringinfo.c
@@ -4,20 +4,38 @@
 #include "matrix.h"
 #include "complex.h"
 
-RingInfo *InitRingInfo(
+#define MAX_RING_INFOS 10
+static RingInfo* ringInfos[MAX_RING_INFOS] = {NULL};
+static int numRingInfos = 0;
+
+RingInfo* InitRingInfo(
         size_t size,
-        void (*sum)(const void * a, const void * b, void * sum),
-        void (*mult)(const void *a , const void * b, void * res),
-        void (*zero)(void * a),
+        void (*sum)(const void *, const void *, void *),
+        void (*mult)(const void *, const void *, void *),
+        void (*zero)(void *),
         void (*input)(Matrix *),
         void (*output)(Matrix *)) {
-    RingInfo *ring = calloc(1, sizeof(RingInfo));
+
+  
+    for (int i = 0; i < numRingInfos; i++) {
+        RingInfo* ring = ringInfos[i];
+        if (ring->size == size && ring->sum == sum && ring->mult == mult &&
+            ring->zero == zero && ring->input == input && ring->output == output) {
+            return ring;
+        }
+    }
+
+    
+    RingInfo* ring = calloc(1, sizeof(RingInfo));
     ring->size = size;
     ring->sum = sum;
     ring->mult = mult;
     ring->zero = zero;
     ring->input = input;
     ring->output = output;
+
+
+    ringInfos[numRingInfos++] = ring;
+
     return ring;
 }
-
